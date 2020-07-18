abeaa8d8ccd9415f2c6a137ab5c9ff2fd84fe22a
suhailahmed1998 <suhailahmed261998@gmail.com>
2020-07-18 19:36:40 +0530

    Complete home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,68 @@
+import 'package:book_rent/signin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MainMaterial());
+}
+class MainMaterial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
+    return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+            child: DrawerHeader(
+              child: Row(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      'https://pbs.twimg.com/media/C27dQJbWQAIgjaR.jpg',
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Hey, Sohail',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+          ),
+          Align(
+              alignment: Alignment.topLeft,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (com) {
+                      return Signin();
+                    }),
+                  );
+                },
+                child: Text("Sign In"),
+              ))
+        ],
+      )),
+      appBar: AppBar(
+        title: Center(
+          child: Text("Book Rent"),
+        ),
+      ),
     );
   }
 }
---
+++ lib/signin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Book Rent"),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
